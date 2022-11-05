1173579f50d9a05e86ce9191876b59d59574f05c
idanigor <idanigor@gmail.com>
2022-11-05 17:23:06 +0600

    feat: add 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
-
+const fs = require('fs')
+const fsPromises = require('fs/promises')
+const { join } = require('path')
+const pathCopy = join(__dirname, 'files-copy')
+const copyDir = () => {
+	fs.stat(pathCopy, (error) => {
+		if (!error) {
+			fs.readdir(pathCopy, (err, files) => {
+				files.forEach((file) => {
+					fsPromises.unlink(join(__dirname, 'files-copy', file))
+				})
+			})
+		}
+		fs.mkdir(pathCopy, () => {
+			fs.readdir(join(__dirname, 'files'), (err, files) => {
+				files.forEach((file) => {
+					fsPromises.copyFile(
+						join(__dirname, 'files', file),
+						join(__dirname, 'files-copy', file)
+					)
+				})
+			})
+		})
+	})
+}
+copyDir()
